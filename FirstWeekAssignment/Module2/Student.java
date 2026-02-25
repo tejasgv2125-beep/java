bae608ab138dc7fb46bd7a47ebaa2c6bd1bd8604
tejasgv2125-beep <tejasgv2125@gmail.com>
2026-02-25 12:11:19 +0530

    Add files via upload

---
+++ FirstWeekAssignment/Module2/Student.java
@@ -0,0 +1,23 @@
+package Module2;
+public class Student {
+	
+    int id;
+    String name;
+    Student() {
+        id = 100;
+        name = "Tejas G V";
+    }
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+    void display() {
+        System.out.println("ID: " + id + " Name: " + name);
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student(101, "Shreya");
+        s1.display();
+        s2.display();
+    }
+}
