7da079e0acfe144f90a146445890de121d0e014c
tejasgv2125-beep <tejasgv2125@gmail.com>
2026-02-25 12:09:06 +0530

    Add files via upload

---
+++ FirstWeekAssignment/Module1/StudentGrade.java
@@ -0,0 +1,29 @@
+package Module1;
+import java.util.Scanner;
+public class StudentGrade {
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter student's marks : ");
+	        int marks = sc.nextInt();
+	        int gradeCategory = marks / 10;
+	        switch (gradeCategory) {
+	            case 10:
+	            case 9:
+	                System.out.println("Grade: A");
+	                break;
+	            case 8:
+	                System.out.println("Grade: B");
+	                break;
+	            case 7:
+	                System.out.println("Grade: C");
+	                break;
+	            case 6:
+	                System.out.println("Grade: D");
+	                break;
+	            default:
+	                System.out.println("Grade: F");
+	        }
+	        sc.close();
+	    }
+	}
