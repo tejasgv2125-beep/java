d66f26fab6b0469d2d952fc7180b72710fcf8076
tejasgv2125-beep <tejasgv2125@gmail.com>
2026-02-26 20:56:51 +0530

    commit

---
+++ Day4/Calculator.java
@@ -0,0 +1,24 @@
+package Day4;
+
+
+//method overloading
+public class Calculator {
+
+    static void calci(int a,int b) {//here method is same so it is method overloading
+        System.out.println(a+b);
+    }
+    static void calci(double a,double b) {//if we use the static keyword we doint need to create the obejct
+        System.out.println(a*b);
+
+    }//static method can be overloaded
+    static  int calci(int a, int b,int c) {
+        return (a+b)/c;
+
+    }
+    public static void main(String[] args) {
+        calci(5,5);
+        calci(10.9,1.9);
+        System.out.println(calci(7,9,13));
+    }
+
+}
